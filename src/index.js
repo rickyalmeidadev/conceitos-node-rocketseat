25857d64efb6a13d8838c513e8bd9a0d7b23542d
Ricky Almeida <ricky.almeida.dev@gmail.com>
2020-08-17 22:17:41 -0300

    feat: add logs of request body, params and query

--- src/index.js
+++ src/index.js
@@ -3,25 +3,46 @@ const morgan = require('morgan');
 
 const app = express();
 
+app.use(express.json());
 app.use(morgan('dev'));
 
-app.get('/projects', (request, response) => response.json([
-  'Projeto 1',
-  'Projeto 2',
-  'Projeto 3',
-]));
+app.get('/projects', (request, response) => {
+  const { title, owner } = request.query;
 
-app.post('/projects', (request, response) => response.json([
-  'Projeto 1',
-  'Projeto 2',
-  'Projeto 3',
-]));
+  console.log('>>> Title: ', title);
+  console.log('>>> Owner: ', owner);
 
-app.put('/projects/:id', (request, response) => response.json([
-  'Projeto 1',
-  'Projeto 2',
-  'Projeto 3',
-]));
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
+
+app.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
+
+  console.log('>>> Title: ', title);
+  console.log('>>> Owner: ', owner);
+
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+  const { id } = request.params;
+
+  console.log('>>> ID: ', id);
+
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
 
 app.delete('/projects/:id', (request, response) => response.json([
   'Projeto 1',
