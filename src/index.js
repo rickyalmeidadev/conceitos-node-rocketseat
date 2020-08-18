4a16803f672b8a8218a0be6c3a18161dca10c5a3
Ricky Almeida <ricky.almeida.dev@gmail.com>
2020-08-17 22:05:04 -0300

    feat: create example routes for http methods

--- src/index.js
+++ src/index.js
@@ -2,8 +2,30 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => response.json({
-  message: 'Hello, World!',
-}));
+app.get('/projects', (request, response) => response.json([
+  'Projeto 1',
+  'Projeto 2',
+  'Projeto 3',
+]));
 
-app.listen(5000);
+app.post('/projects', (request, response) => response.json([
+  'Projeto 1',
+  'Projeto 2',
+  'Projeto 3',
+]));
+
+app.put('/projects/:id', (request, response) => response.json([
+  'Projeto 1',
+  'Projeto 2',
+  'Projeto 3',
+]));
+
+app.delete('/projects/:id', (request, response) => response.json([
+  'Projeto 1',
+  'Projeto 2',
+  'Projeto 3',
+]));
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000...');
+});
