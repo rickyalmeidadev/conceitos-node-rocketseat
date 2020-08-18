c211e9366fe29e6dd24fb0810827568c578d65b0
Ricky Almeida <ricky.almeida.dev@gmail.com>
2020-08-17 22:32:36 -0300

    feat: create logic for a very simple crud

--- src/index.js
+++ src/index.js
@@ -1,54 +1,62 @@
 const express = require('express');
 const morgan = require('morgan');
+const { uuid } = require('uuidv4');
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
 
+const projects = [];
+
 app.get('/projects', (request, response) => {
-  const { title, owner } = request.query;
-
-  console.log('>>> Title: ', title);
-  console.log('>>> Owner: ', owner);
-
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  return response.json(projects);
 });
 
 app.post('/projects', (request, response) => {
   const { title, owner } = request.body;
 
-  console.log('>>> Title: ', title);
-  console.log('>>> Owner: ', owner);
+  const project = { id: uuid(), title, owner };
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  projects.push(project);
+
+  return response.json(project);
 });
 
 app.put('/projects/:id', (request, response) => {
   const { id } = request.params;
+  const { title, owner } = request.body;
 
-  console.log('>>> ID: ', id);
+  const projectIndex = projects.findIndex((project) => project.id === id);
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  if (projectIndex < 0) {
+    return response.status(400).json({
+      error: 'Project not found.',
+    });
+  }
+
+  const project = { id, title, owner };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
 });
 
-app.delete('/projects/:id', (request, response) => response.json([
-  'Projeto 1',
-  'Projeto 2',
-  'Projeto 3',
-]));
+app.delete('/projects/:id', (request, response) => {
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({
+      error: 'Project not found.',
+    });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
+});
 
 app.listen(5000, () => {
   console.log('Server is running on port 5000...');
